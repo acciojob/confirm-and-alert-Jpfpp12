882be5deb21d17937cc42efbd799b566b38b3705
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-04-14 19:42:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+window.confirm("Do you want to proceed ?")
+if (window.confirm==true) {
+	alert("You clicked OK. Proceeding...")
+}else{
+	alert("You clicked Cancel. Exiting...")
+}
